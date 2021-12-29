553af9078e1bedcc0cbc907b63b6fad696fe0e03
Akkadate <akkadate@gmail.com>
2021-12-29 11:03:49 +0700

    dist/index.html

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 const http = require('http');
 const port = 3000;
+const fs = require('fs');
 
 http.createServer(render).listen(port)
 console.log(`Server Start on port:${port} <Ctrl + C> to Stop`);
 
 function render(request, response) {
-    response.writeHead(200, {'Content-Type':'text/html'})
-    let html =`
-    <!Doctype html>
-    <html>
-    <head>
-    <title> Node.js</title>
-    </head>
-    <body>
-        <h2>Welcome to Node.js</h2>
-        <b>Node.js run JavaScript at Server Side</b>
-    </body>
-    </html>
-    `
-    response.write(html);
-    response.end();
+    let ctype = { 'Content-Type': 'text/html' }
+    fs.readFile('../dist/index.html', (error, content) => {
+        if (!error){
+            response.writeHead(200, ctype)
+            response.write(content)
+        } else {
+            response.writeHead(404,ctype)
+            response.write(error.message)
+        }
+        return response.end()
+    })
 }
