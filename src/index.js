f9f15b13a97c719486daed33ed9ea4b44df51ca3
Akkadate <akkadate@gmail.com>
2021-12-29 13:23:19 +0700

    p248

--- src/index.js
+++ src/index.js
@@ -1,32 +1,21 @@
-const http = require('http');
-const port = 3000;
-const fs = require('fs');
+const app = reqire('express')()
+app.use((request, response) => {
+    response.status(200)
+    response.type('text/html')
+    response.send(`
+   <!doctype html>
+    <html lang="en">
 
-http.createServer(render).listen(port)
-console.log(`Server Start on port:${port} <Ctrl + C> to Stop`);
-
-function render(request, response) {
-    let url =request.url
-    url =(url.endsWith('/')) ? url : url + '/'
-    
-    let fileName ='../dist/'   //ไฟล์ที่เก็บอยู่ในโฟลเดอร์ html
-    switch (url) {
-        case '/': fileName += 'index.html' ; break
-        case '/about/' : fileName += 'about.html' ; break
-        case '/product/' : fileName += 'product.html' ; break
-        case '/blog/' : fileName += 'blog.html' ; break
-    }
-
-   
-    fs.readFile(fileName, (error, content) => {
-        let ctype ={'Content-Type' : 'text/html'}
-        if (!error){
-            response.writeHead(200, ctype)
-            response.write(content)
-        } else {
-            response.writeHead(404,ctype)
-            response.write(error.message)
-        }
-        return response.end()
-    })
-}
+<body>
+    <h2>Welcome to Node.js</h2>
+    <ul>
+        <li>Node.js is JavaScript Runtime Enviroment</li>
+        <li>Node.js uses Google V8 engine</li>
+    </ul>
+ 
+</body>
+</html>
+    `)
+})
+app.listen(3000,() =>
+    console.log('server start....'))
