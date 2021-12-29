dca7d051d50e4f85c32e40f297c125b1d0288fa3
Akkadate <akkadate@gmail.com>
2021-12-29 10:38:33 +0700

    edit index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ const http = require('http');
 const port = 3000;
 
 http.createServer(render).listen(port)
-
-console.log(`Server Start on port:${port} <Ctrl + C to Stop`);
+console.log(`Server Start on port:${port} <Ctrl + C> to Stop`);
 
 function render(request, response) {
-    response.write('Hello Node by akkadate')
-    response.end()
+    response.writeHead(200, {'Content-Type':'text/html'})
+    let html =`
+    <!Doctype html>
+    <html>
+    <head>
+    <title> Node.js</title>
+    </head>
+    <body>
+        <h2>Welcome to Node.js</h2>
+        <b>Node.js run JavaScript at Server Side</b>
+    </body>
+    </html>
+    `
+    response.write(html);
+    response.end();
 }
